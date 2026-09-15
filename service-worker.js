0616c93abc4b4390a2ccdb074254713c1870e590
MKTO46 <72702490+MKTO46@users.noreply.github.com>
2026-09-15 12:42:49 -0400

    Refresh Kitty Quest offline cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kitty-quest-v2';
+const CACHE='kitty-quest-v4';
 const FILES=['./','./index.html','./manifest.webmanifest','./app-icon.png','./galaxy-background.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
@@ -10,3 +10,4 @@ self.addEventListener('fetch',event=>{
     return response;
   }).catch(()=>caches.match(event.request).then(response=>response||caches.match('./index.html'))));
 });
+
